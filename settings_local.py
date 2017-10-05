0c98e5ee3e565e3a4d8022aa35a0d986512f0139
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-10-05 09:23:58 +0300

    Set static url for the setup

--- settings_local.py
+++ settings_local.py
@@ -5,3 +5,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CONTAINER_MODE = True
 CONTAINER_SCRIPT = os.path.join(BASE_DIR, "scripts/docker-compose-run.sh")
 
+STATIC_URL = "http://localhost:8080/static/"
+
