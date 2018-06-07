b3f8032e5cc9919ae875f7d0d2faef3b5efc577a
Markku Riekkinen <markku.riekkinen@aalto.fi>
2018-06-07 18:14:33 +0300

    Remove the personal directory mount from grader containers (unwanted feature and not needed for normal personalized exercises). Use the GRADER_PERSONALIZED_CONTENT_PATH env variable to read the path to the stored personalized instance files.

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -32,26 +32,17 @@ parse_val_from_exercise_json () {
 
 # in personalized exercises, we need the file path to the correct instance
 PERSONALIZED_MOUNT=$(parse_val_from_exercise_json personalized_exercise)
-# personal directory is an additional feature in the personalization, but it might
-# be removed in the future since it is hacky and not really needed
-PERSONAL_DIR_MOUNT=$(parse_val_from_exercise_json personal_directory)
 
 # normal exercises do not use the personalized mount
 PERSONALIZED_ARG=''
 if [ -n "$PERSONALIZED_MOUNT" ]; then
-  TMP_PERSONALIZED_MOUNT=$TMP/_personalized/${PERSONALIZED_MOUNT##/srv/data/mgrader_ex-meta/}
+  TMP_PERSONALIZED_MOUNT=$TMP/_personalized/${PERSONALIZED_MOUNT##$GRADER_PERSONALIZED_CONTENT_PATH}
   rm -rf $TMP_PERSONALIZED_MOUNT
   mkdir -p $TMP_PERSONALIZED_MOUNT
   cp -r $PERSONALIZED_MOUNT $(dirname $TMP_PERSONALIZED_MOUNT)
   PERSONALIZED_ARG="-v $TMP_PERSONALIZED_MOUNT:/personalized_exercise"
 fi
-if [ -n "$PERSONAL_DIR_MOUNT" ]; then
-  TMP_PERSONAL_DIR_MOUNT=$TMP/_personalized/${PERSONAL_DIR_MOUNT##/srv/data/mgrader_ex-meta/}
-  rm -rf $TMP_PERSONAL_DIR_MOUNT
-  mkdir -p $TMP_PERSONAL_DIR_MOUNT
-  cp -r $PERSONAL_DIR_MOUNT $(dirname $TMP_PERSONAL_DIR_MOUNT)
-  PERSONALIZED_ARG="$PERSONALIZED_ARG -v $TMP_PERSONAL_DIR_MOUNT:/personal_directory"
-fi
+
 
 docker run \
   -d --rm \
@@ -63,9 +54,4 @@ docker run \
   --network=aplus_default \
   $DOCKER_IMAGE \
   $CMD
-
-# update the contents of the personal directory after the grader has exited
-if [ -n "$PERSONAL_DIR_MOUNT" ]; then
-  cp -r $TMP_PERSONAL_DIR_MOUNT/* $PERSONAL_DIR_MOUNT
-fi
 
