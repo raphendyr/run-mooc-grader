f4eb10ace66a1e5987780853ec1e5fd014150b01
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2019-01-22 15:23:18 +0200

    Do not die if default course is missing

--- rootfs/srv/grader-init.sh
+++ rootfs/srv/grader-init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eu
 
 # generate instances for personalized exercises (course key default)
-setuidgid $USER python3 manage.py pregenerate_exercises --gen-if-none-exist default
+setuidgid $USER python3 manage.py pregenerate_exercises --gen-if-none-exist default || true
